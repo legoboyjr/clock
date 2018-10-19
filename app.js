a39b8f4c15e8ab8a515bc7364a0f27f3482e81cb
Alexander Thompson <legoboyjr@gmail.com>
2018-10-19 19:04:39 -0400

    skeliton

--- app.js
+++ app.js
@@ -3,15 +3,26 @@ const clock = document.querySelector('#clock');
 const display = clock.querySelector('#display');
 const hour = display.querySelector("#hours");
 const minutes = display.querySelector("#minutes");
+const date = clock.querySelector('#date');
 let time = new Date();
 // event bindings
 
 // functions
 function tick() {
+    //get time
     const minValue = time.getMinutes();
     const hourValue = time.getHours();
+    //render time
     hours.textContent = hourValue;
     minutes.textContent = `${minValue}`.padStart(2, '0');
+
+    //get date (format: MM-DD-YYYY)
+    const year = time.getFullYear();
+    const month = time.getMonth();
+    const day = time.getDate();
+    //render date
+    date.textContent = `${month}-${day}-${year}`;
+    //update time and time
     time = new Date();
 }
 
