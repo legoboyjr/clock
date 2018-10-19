9671d7d2e0002a216702bc0ed319866f1b5943f3
Alexander Thompson <legoboyjr@gmail.com>
2018-10-18 20:35:23 -0400

    skeliton

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("It works!!!!!!!");
+// variable declorations
+const clock = document.querySelector('#clock');
+const display = clock.querySelector('#display');
+const hour = display.querySelector("#hours");
+const minutes = display.querySelector("#minutes");
+let time = new Date();
+// event bindings
+
+// functions
+function tick() {
+    const minValue = time.getMinutes();
+    const hourValue = time.getHours();
+    hours.textContent = hourValue;
+    minutes.textContent = `${minValue}`.padStart(2, '0');
+    time = new Date();
+}
+
+// Kick it off
+tick();
+setInterval(tick, 1000);
