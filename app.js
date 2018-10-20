bdd6250dcccb54e8d4edb50018f3abeeb1e389e0
Alexander Thompson <legoboyjr@gmail.com>
2018-10-19 20:15:22 -0400

    skeliton

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const display = clock.querySelector('#display');
 const hour = display.querySelector("#hours");
 const minutes = display.querySelector("#minutes");
 const date = clock.querySelector('#date');
+const amPm = display.querySelector('#am-pm');
 let time = new Date();
+let isPm = time.getHours() >= 12;
 // event bindings
 
 // functions
@@ -13,7 +15,7 @@ function tick() {
     const minValue = time.getMinutes();
     const hourValue = time.getHours();
     //render time
-    hours.textContent = hourValue;
+    hours.textContent = hourValue % 12 === 0 ? 12 : hourValue % 12;
     minutes.textContent = `${minValue}`.padStart(2, '0');
 
     //get date (format: MM-DD-YYYY)
@@ -22,8 +24,17 @@ function tick() {
     const day = time.getDate();
     //render date
     date.textContent = `${month}-${day}-${year}`;
+    //render am/pm status
+    amPm.classList.remove('am','pm');
+    if (isPm) {
+        amPm.classList.add('pm');
+    } else {
+        amPm.classList.add('am');
+    }
     //update time and time
     time = new Date();
+    //update am/pm
+    isPm - time.getHours() >= 12;
 }
 
 // Kick it off
